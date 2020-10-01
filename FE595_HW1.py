d7ec44911bc05f10703c7e3fa12518258ced9a42
ben062930 <70728503+ben062930@users.noreply.github.com>
2020-10-01 13:20:35 -0400

    Update FE595_HW1.py

--- FE595_HW1.py
+++ FE595_HW1.py
@@ -1,10 +1,11 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-x = np.arange(0, 2*np.pi, 0.0001*np.pi)
-
-plt.plot(x, np.sin(x), label = 'Sin')
-plt.plot(x, np.cos(x), label = 'Cos')
-plt.legend()
+a = np.arange(0, 2*np.pi, 0.001)
+plt.plot(a, np.cos(a), color = "green", label = "SIN")
+plt.plot(a, np.sin(a), color = "yellow", label = "COS")
+# plt.show()
+plt.plot(a, np.tan(a), label = "TAN", color = "red")
+plt.ylim(-10,10)
 
 plt.show()
