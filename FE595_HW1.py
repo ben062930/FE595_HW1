35045ea261fdffb50c587229ce0aa09c6bfdaa1f
ben062930 <70728503+ben062930@users.noreply.github.com>
2020-11-12 20:54:32 -0500

    Update FE595_HW1.py

--- FE595_HW1.py
+++ FE595_HW1.py
@@ -5,7 +5,7 @@ x = np.arange(0, 2*np.pi, 0.0001*np.pi)
 
 plt.plot(x, np.sin(x), label = 'Sin')
 plt.plot(x, np.cos(x), label = 'Cos')
-plt.plot(a, np.tan(a), label = "Tan", color = "red")
+plt.plot(x, np.tan(x), label = "Tan", color = "red")
 plt.ylim(-10,10)
 plt.legend()
 
